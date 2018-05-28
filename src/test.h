0f6f7262562bdbc5f2ef55fcb303bd01d922197d
David Warring <david.warring@gmail.com>
2018-05-28 17:54:34 +1200

    generalize slightly for windoze

--- src/test.h
+++ src/test.h
@@ -1,1 +1,12 @@
-extern void ptr_to_strs (char ***ptr);
+#ifndef TEST_H_
+#define TEST_H_
+
+#ifdef _WIN32
+#define DLLEXPORT __declspec(dllexport)
+#else
+#define DLLEXPORT extern
+#endif
+
+#endif
+
+DLLEXPORT void ptr_to_strs (char ***ptr);
